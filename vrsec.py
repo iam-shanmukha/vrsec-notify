08228d87b2799586fffca65109dff541934ce4b2
Shanmukha Vishnu <50124557+iam-shanmukha@users.noreply.github.com>
2019-11-01 05:53:50 +0530

    Updated url
    Updated URL according to new changes of vrsiddhartha.ac.in

--- vrsec.py
+++ vrsec.py
@@ -12,17 +12,24 @@ import os
 import time
 from selenium import webdriver
 
-url = "http://www.vrsiddhartha.ac.in/index.php/component/content/category/3"
-#url = "http://shanmukhavishnu.in"
+url = "http://www.vrsiddhartha.ac.in/examination-archives/"
+#url = "http://WWW.shanmukhavishnu.in"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 #print(soup)
 search="VR17"
 results1 = soup.body.find_all(string=re.compile('.*{0}.*'.format(search)), recursive=True)
+#Searching for VR17 and store len(results1) in variable sto. In next step, recusively search for VR17. if Results declared new "VR17" added to site and so len(resulst)> sto.
+#Hence opens Examination link
 sto = len(results1)
+#print(sto)
 while True:
 	print("Waiting for Results to be Declared")
+	page = requests.get(url)
+	soup = BeautifulSoup(page.text, 'html.parser')
+	search="VR17"
 	results = soup.body.find_all(string=re.compile('.*{0}.*'.format(search)), recursive=True)
+	#print(len(results)) At the time of annoucing results if any new term of VR17 added to the site, it will reflected here
 	if len(results) > sto:
 		subprocess.Popen(['notify-send', "Results Declared"])
 		subprocess.Popen(['notify-send', "Opening Results Page"])
